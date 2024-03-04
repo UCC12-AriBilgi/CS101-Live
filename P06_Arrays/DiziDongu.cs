a8af574099ded8f0ca8d26906a7afeba146e8166
ukaracivio <umit.karacivi@outlook.com>
2024-03-04 14:32:15 +0300

    some changes

--- P06_Arrays/DiziDongu.cs
+++ P06_Arrays/DiziDongu.cs
@@ -53,33 +53,86 @@ namespace P06_Arrays
             // Nötrlerin adedi : ... tane
             // diye
 
-            int[] sayilar = {4,5,-15,22,-34,3,0,43,100 };
+            //int[] sayilar = {4,5,-15,22,-34,3,0,43,100 };
+            //int negadet = 0;
+            //int pozadet = 0;
+            //int notradet = 0;
 
-            // foreach kullanın
-            foreach (int sayi in sayilar)
-            {
-                // gelen sayinın büyük 0 mı küçük 0 mı olduğunu anlamam lazım ki yanına hangi türden olduğunu yazabileyim.
+            //// foreach kullanın
+            //foreach (int sayi in sayilar)
+            //{
+            //    // gelen sayinın büyük 0 mı küçük 0 mı olduğunu anlamam lazım ki yanına hangi türden olduğunu yazabileyim.
 
-                if (sayi > 0)
-                {
-                    Console.WriteLine(sayi + "\tPozitifdir..");
-                }
+            //    if (sayi > 0)
+            //    {
+            //        Console.WriteLine(sayi + "\tPozitifdir..");
+            //        pozadet++;
+            //    }
 
-                else if (sayi < 0)
-                {
-                    Console.WriteLine(sayi + "\tNegatifdir..");
+            //    else if (sayi < 0)
+            //    {
+            //        Console.WriteLine(sayi + "\tNegatifdir..");
+            //        negadet++;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(sayi + "\tNötrdür..");
+            //        notradet++;
+            //    }
+            //}
 
-                }
-                else
-                {
-                    Console.WriteLine(sayi + "\tNötrdür..");
-
-                }
-            }
-
+            //Console.WriteLine($"Pozitif sayıların adedi : {pozadet}");
+            //Console.WriteLine($"Negatif sayıların adedi : {negadet}");
+            //Console.WriteLine($"Nötr sayıların adedi : {notradet}");
 
             #endregion
 
+            #region Hack03
+            //Kullanıcıdan alınan bir metin içindeki sesli harf sayısını bulan ve bulduklarını baska bir diziye yerlestiren p.parçacığını tasarlayınız.
+
+            // Öncelike seslilerin olduğu bir dizi tanımı yapıyorum.
+            char[] sesliler = {'a','e','ı','i','o','ö','u','ü' };
+
+            // Bulacağım seslileri yerleştireceğim bir dizi
+            char[] hangileri = new char[20];
+
+
+            string metin = ""; // ekrandan alacağım metin bilgisi
+            int hindex = 0; // hangileri dizisinin index bilgisini tutacak
+            int adet = 0; // bulduğum sesli harf adedini tutacak.
+
+            Console.WriteLine("Lütfen metni giriniz :");
+            metin = Console.ReadLine().ToLower();
+            // Büyük harf girse bile küçüğe çevirdim
+
+            // Not : aslında geriplanda metin bilgisi de bir dizi gibi algılabilir
+
+            // sesliler diziyle işe başlayacağız.
+            // 1.for cevrimi sesliler dizisi üzerinde hareket edecek
+            for (int sesliindex = 0; sesliindex < sesliler.Length ; sesliindex++)
+            {
+                // 2.for çevrimi metin bilgisi için
+                // metinin basından sonuna kadar ilerlenicek
+                for (int metinindex = 0; metinindex < metin.Length; metinindex++)
+                {
+                    // eğer metin içindekiş bir karakter benim sesli içindeki karaktere uyuyor mu
+                    if (metin[metinindex] == sesliler[sesliindex])
+                    {
+                        // baska bir diziye tasıyayım.
+                        hangileri[hindex] = metin[metinindex];//bulduğum sesli harfin ne olduğunu metin[metinindex] ile bilebiliyorum.
+                        hindex++; // birsonraki eleman için
+
+                        adet++; // Bulduğum sesli harf adedini artırıyorum
+                    }
+                }
+
+                // İç içe for olayında en önce içteki for biter sonra dıştaki for a dönülür ve o biter
+            }
+
+            Console.WriteLine($"Girmiş olduğunuz metinde {adet} adet sesli harf vardır\n\n");
+            Console.WriteLine(hangileri);
+
+            #endregion
 
 
 

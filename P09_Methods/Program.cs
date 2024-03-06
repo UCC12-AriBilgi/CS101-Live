ca3aa940716fa8db341b321aed6a480c3a5fb09d
ukaracivio <umit.karacivi@outlook.com>
2024-03-06 12:00:31 +0300

    Some changes

---
+++ P09_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P09_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1.Main(args);
+
+
+        }
+    }
+}
---
+++ P09_Methods/Sample1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09_Methods
+{
+    internal class Sample1
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

2b28f5c09bbc737519c9004ad10c52b3bba5ef93
ukaracivio <umit.karacivi@outlook.com>
2024-03-06 12:25:05 +0300

    Some changes

--- P09_Methods/Sample1.cs
+++ P09_Methods/Sample1.cs
@@ -10,11 +10,60 @@ namespace P09_Methods
     {
         public static void Main(string[] args)
         {
+            // Hack01
+            // Dikdörtgen alanını hesaplayan bir metodun 4 farklı seklinin incelenmesi
+
+            #region 1. Geriye değer döndürmeyen ve parametre almayan metot
+
+            //Hesapla();
+
+            #endregion
+
+            #region 2. Geriye değer döndürmeyen ama parametre alan metot
+            // Kullanıcı girişi gibi işlemler burada...
+            // hesap kitap metotda olacak ...dolayısıla buradan almış olduğum bilgileri parametre olarak metoda göndereceğim
+            Console.WriteLine("Lütfen kısa kenarı giriniz :");
+            int kk = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen uzun kenarı giriniz :");
+            int uk = Convert.ToInt32(Console.ReadLine());
+
+            Hesapla(kk, uk);
+
+            #endregion
+
 
 
 
 
             Console.ReadKey();
         }
+
+        #region Methodlar
+        // region 1
+        private static void Hesapla()
+        {
+            Console.WriteLine("Lütfen kısa kenarı giriniz :");
+            int kk=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen uzun kenarı giriniz :");
+            int uk=Convert.ToInt32(Console.ReadLine());
+
+            int alan = kk * uk;
+
+            Console.WriteLine($"\nDikdörtgenin alanı = {alan}");
+
+
+        }
+
+        // region 2
+        private static void Hesapla(int pkisakenar,int puzunkenar)
+        {
+            int alan=pkisakenar * puzunkenar;
+            
+            Console.WriteLine($"\nDikdörtgenin alanı = {alan}");
+        }
+
+        #endregion
     }
 }
